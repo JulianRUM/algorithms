3db316f6d287159cf4baac1608356a45d14b8bbd
julian-ux227 <jurenamarte34@gmail>
2021-12-13 09:35:53 -0500

    algorithm 1

---
+++ exercise_1.js
@@ -0,0 +1,14 @@
+
+/* Escriba un programa al cual se le digite un número e imprima
+por consola su nombre cuantas
+veces como lo indique el número digitado */
+
+const num = 7;
+const nom = "Julian";
+
+// Ciclo
+for(let i=0; i < num; ++i){
+    
+    console.log(nom);
+}
+
