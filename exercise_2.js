287cb69fba70a8e38d5dc552f394ddd7695525b9
julian-ux227 <jurenamarte34@gmail>
2021-12-15 09:20:02 -0500

    Algoritmo 2

---
+++ exercise_2.js
@@ -0,0 +1,10 @@
+/* Haga un programa al cual se le digite
+un número entero y despliegue por pantalla
+la suma de  los números comprendidos entre 0 y el número digitado.*/
+
+let num = 9;
+let sum = 0;
+
+num = 7;
+let sum_gauss = num * (num + 1)/2
+console.log(sum_gauss); 
